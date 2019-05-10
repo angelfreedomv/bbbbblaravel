cf0d7b1982b77a74c515d1db0332ecc0fa744784
angelfreedomv <echopan1234@gmail.com>
2019-05-10 06:37:41 +0000

    修复跳转链接

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
+
 function route_class()
 {
-    return str_replace('.','-',Route::currentRouteName());
+    return str_replace('.', '-', Route::currentRouteName());
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,3 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
