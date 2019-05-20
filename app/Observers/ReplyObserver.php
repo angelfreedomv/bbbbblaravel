5d4e5a9eed16a02d7bc9f484833f54f1180cc3cc
angelfreedomv <echopan1234@gmail.com>
2019-05-20 02:22:40 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -5,10 +5,6 @@ namespace App\Observers;
 use App\Models\Reply;
 use App\Notifications\TopicReplied;
 
-
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
-
 class ReplyObserver
 {
     public function created(Reply $reply)
@@ -19,6 +15,7 @@ class ReplyObserver
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
     }
+
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
